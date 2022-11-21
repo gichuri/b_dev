e4266c8a618bc499e372e7e5143868919be0156c
martin ndungu <M.ndungu@alustudent.com>
2022-11-21 23:57:22 +0300

    updates

---
+++ app.js
@@ -0,0 +1,16 @@
+const sections = document.querySelectorAll('section');
+const sectBtns = document.querySelectorAll('controls');
+const sectBtn = document.querySelectorAll('control');
+const allsections = document.querySelector('.main-content');
+
+
+function PageTransitions(){
+    //what happens when I click on button
+    for(let i=0; i < sectBtn.length; i++){
+        sectBtn[i].addEventListener('click', () =>{
+            let currentBtn = document.querySelectorAll('.active-btn');
+            currentBtn[]
+        })
+    }
+
+}
