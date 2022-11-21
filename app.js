f648ed6f328be645b2b8d385faa643caae6a7151
martin ndungu <M.ndungu@alustudent.com>
2022-11-22 00:13:15 +0300

    js start

--- app.js
+++ app.js
@@ -6,11 +6,14 @@ const allsections = document.querySelector('.main-content');
 
 function PageTransitions(){
     //what happens when I click on button
-    for(let i=0; i < sectBtn.length; i++){
-        sectBtn[i].addEventListener('click', () =>{
+    for(let i = 0; i < sectBtn.length; i++){
+        sectBtn[i].addEventListener('click', function(){
             let currentBtn = document.querySelectorAll('.active-btn');
-            currentBtn[]
+            currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
+            this.className += ' active-btn ';
         })
     }
 
 }
+
+PageTransitions();
